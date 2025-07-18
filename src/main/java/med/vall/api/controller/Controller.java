5f73a6a6a0331d51b3964679a944efcb8cf5e76e
Alecio <aleciosilveira97@gmail.com>
2025-07-17 23:53:29 -0300

    Ajuste na controller

--- src/main/java/med/vall/api/controller/Controller.java
+++ src/main/java/med/vall/api/controller/Controller.java
@@ -6,6 +6,9 @@ import med.vall.api.DTO.MedicosListaDTO;
 import med.vall.api.entity.EntityMedico;
 import med.vall.api.repository.RepositoryMedico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,7 +25,8 @@ public class Controller {
         repositoryMedico.save(new EntityMedico(medicosDTO));
     }
    @GetMapping
-    public List<MedicosListaDTO> medicosListaDTOS(){
-        return  repositoryMedico.findAll().stream().map(MedicosListaDTO::new).toList();
+   // Serve para configurar a quantidades de paginas e ordem que executa o json @PageableDefault(size = 10, sort = {"nome"}
+    public Page<MedicosListaDTO> medicosListaDTOS(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable){
+        return  repositoryMedico.findAll(pageable).map(MedicosListaDTO::new);
    }
 }

4af3bf4c46eb367c463f2d856f6b707ad6cf8cec
Alecio <aleciosilveira97@gmail.com>
2025-08-07 21:48:50 -0300

    Configurando bando de dados de teste

---
+++ src/test/java/med/vall/api/repository/RepositoryMedicoTest.java
@@ -0,0 +1,18 @@
+package med.vall.api.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class RepositoryMedicoTest {
+
+    @Test
+    void escolherMedicoAleatorio() {
+    }
+}
